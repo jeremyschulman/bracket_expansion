e6a62d524f07285f279ce72da12aa38082eaef2f
Jeremy Schulman <nwkautomaniac@gmail.com>
2015-02-18 10:38:26 -0500

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="bracket_expansion",
+    version="0.1.0",
+    author="Jeremy Schulman",
+    author_email="nwkautomaniac@gmail.com",
+    description=("string generate for bracket patters"),
+    license="GNU GPLv3",
+    url="https://github.com/jeremyschulman/bracket_expansion",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking'
+    ],
+)
